0429c61be30ce2a51ffb198ee8002717dfd4fb3f
jaswinder Kumar <kumarsanju54@gmail.com>
2024-03-26 14:11:18 +0000

    findthefrequencry

--- arrays/frequencyOfEele.js
+++ arrays/frequencyOfEele.js
@@ -1,1 +1,12 @@
 
+let arr  = [10,10,20,11,23,23,23,23,23];
+let obj = {}
+arr.map(ele=>{
+    console.log(obj[ele])
+    if(typeof obj[ele] == "undefined" ){
+        obj[ele] = 1
+    }else{
+        obj[ele]++
+    }
+});
+console.log(obj) //{ '10': 2, '11': 1, '20': 1, '23': 5 }
